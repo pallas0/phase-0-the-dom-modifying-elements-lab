81446e6287b4d20ebcb9119c4cd45d05d3fb1a70
amelia.risner0@gmail.com <arisner@berkeley.edu>
2022-01-01 16:51:39 -0800

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+const mainElem = document.getElementById("main");
+
+mainElem.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.id = 'victory';
+document.body.appendChild(newHeader);
+
+newHeader.innerHTML = "Amelia is the champion";
